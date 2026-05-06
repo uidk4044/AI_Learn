da44acfef23562cb58a1c63388a9a717ef14fbd4
Wang Wei (uig06438) <wei.8.wang@continental-corporation.com>
2026-05-06 17:33:31 +0800

    docker hub使用commit id

--- projects/project1-ci-cd/app/app.py
+++ projects/project1-ci-cd/app/app.py
@@ -1,10 +1,39 @@
+print("🔥 NEW CODE RUNNING")
+
 from flask import Flask
+import psycopg2
+import time
+from datetime import datetime
+import pytz
+
+tz = pytz.timezone("Asia/Shanghai")
+
+print("BUILD TIME:", datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S"))
 
 app = Flask(__name__)
 
+
+def check_db():
+    try:
+        conn = psycopg2.connect(
+            host="db",          # 👈 docker-compose 服务名
+            database="mydb",
+            user="user",
+            password="password"
+        )
+        print("✅ Connected to DB")
+        conn.close()
+    except Exception as e:
+        print("❌ DB connection failed:", e)
+
+
 @app.route("/")
 def home():
-    return "Hello DevOps v2 🚀"
+    return "Hello, DevOps with Docker Compose!"
+
 
 if __name__ == "__main__":
+    # 👉 启动时尝试连接数据库（当前阶段）
+    check_db()
+
     app.run(host="0.0.0.0", port=3000)
