d08a70da043459fd4797aad33c5460b72e07f72a
Wang Wei (uig06438) <wei.8.wang@continental-corporation.com>
2026-05-28 13:52:10 +0800

    自动切换blue green

---
+++ scripts/switch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+TARGET=$1
+
+if [ "$TARGET" == "blue" ]; then
+    PORT=3001
+else
+    PORT=3002
+fi
+
+echo "Switching nginx to $TARGET ($PORT)"
+
+sed -i "s/server host.docker.internal:300[12]/server host.docker.internal:$PORT/" nginx/default.conf
+
+docker exec nginx nginx -s reload
