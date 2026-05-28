d03449adf4be8513e22b7801dc88aae1203528b0
Wang Wei (uig06438) <wei.8.wang@continental-corporation.com>
2026-05-28 14:16:24 +0800

    解决换行符报错

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -13,3 +13,4 @@ echo "Switching nginx to $TARGET ($PORT)"
 sed -i "s/server host.docker.internal:300[12]/server host.docker.internal:$PORT/" nginx/default.conf
 
 docker exec nginx nginx -s reload
+
